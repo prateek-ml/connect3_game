c11008177d2fd4b2bf3155ceb5e8fe8febc75ea3
prateek-ml <prateekb1912@gmail.com>
2020-12-26 18:45:48 +0530

    Game algorithm defined

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -5,17 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
   public class MainActivity extends AppCompatActivity {
+
+      // 0 === Yellow ; 1 === Red; 2 === Empty
+      int activePlayer = 0;
+
+      int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+      int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+                                  {0,3,6}, {1,4,7}, {2,5,8},
+                                  {0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer;
+
         counter.setTranslationY(-1500);
 
-        counter.setImageResource(R.drawable.yellow);
+        if(activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        }
 
+        else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+        for(int[] winPosition : winningPositions)
+        {
+            if(gameState[winPosition[0]] == gameState[winPosition[1]]
+                    && gameState[winPosition[1]] == gameState[winPosition[2]]
+                    && gameState[winPosition[0]] != 2) {
+                // Someone has won.
+                Toast.makeText(this, "Someone has won", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
