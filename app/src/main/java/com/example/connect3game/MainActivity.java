6ae175634d81e40037b4d9e8bd105bb10573561d
prateek-ml <prateekb1912@gmail.com>
2020-12-26 18:24:13 +0530

    Counters animated entry added

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -3,8 +3,20 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+  public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
